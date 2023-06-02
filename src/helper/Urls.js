b1df2af0852a09d45cd4bb29e24d82d363c94252
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2023-06-02 16:41:09 -0500

    cambios

--- src/helper/Urls.js
+++ src/helper/Urls.js
@@ -1,8 +1,8 @@
 import configEnv from '../../config/configEnv';
 
 
-//const BASE = configEnv.BASE || "http://localhost:5000";
-const BASE = process.env.URL_API || 'https://jtm-api.herokuapp.com'; //tesitng env
+const BASE = configEnv.BASE || "http://localhost:5000";
+//const BASE = process.env.URL_API || 'https://jtm-api.herokuapp.com'; //tesitng env
 //const BASE = process.env.URL_API || 'https://api-prod.herokuapp.com';
 
 export const BRANCH = "2301-init";
--- src/views/app/dashboard/CatalogoEjericios.vue
+++ src/views/app/dashboard/CatalogoEjericios.vue
@@ -10,7 +10,7 @@
       <template v-slot:activator="{ on, attrs }">
         <v-btn @click="iniciarCarga" color="primary" v-bind="attrs" v-on="on">
           <v-icon class="caption">mdi-plus</v-icon>
-          Nuevo
+          Agregar
         </v-btn>
       </template>
 
@@ -42,7 +42,6 @@
                     prepend-icon="mdi-image-area"
                     hide-details=""
                     required
-                    
                     :error="errorFileInput"
                     :error-messages="errorMessageFileInput"
                   >
@@ -78,21 +77,18 @@
                 <v-card-text>
                   <v-row align="center" justify="space-around">
                     <v-switch
-                      v-model="ex11"
                       label="BASIC"
                       color="red"
                       value="red"
                       hide-details
                     ></v-switch>
                     <v-switch
-                      v-model="ex11"
                       label="INTERMEDIO"
                       color="red darken-3"
                       value="red darken-3"
                       hide-details
                     ></v-switch>
                     <v-switch
-                      v-model="ex11"
                       label="AVANZADO"
                       color="red"
                       value="red"
@@ -105,7 +101,9 @@
           </v-card-text>
           <v-card-actions class="justify-end">
             <v-btn @click="dialog.value = false">Cerrar</v-btn>
-            <v-btn color="primary" @click="subir">Guardar</v-btn>
+            <v-btn color="primary" @click="subir" :loading="loadingUpload">
+              Guardar
+            </v-btn>
           </v-card-actions>
         </v-card>
       </template>
@@ -134,22 +132,110 @@
         </div>
       </v-col>
     </v-row>
-    
-    <v-row>
-      <v-col cols="12" md="6" lg="3" sm="6" v-for="row in lista" :key="row.id">
-        <base-card class="overflow-hidden">
+
+    <v-container v-if="loading">
+      <v-row>
+        <v-col cols="12" md="6" lg="3" sm="6">
+          <v-skeleton-loader
+            class="mx-auto"
+            max-width="300"
+            type="card"
+          ></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" md="6" lg="3" sm="6">
+          <v-skeleton-loader
+            class="mx-auto"
+            max-width="300"
+            type="card"
+          ></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" md="6" lg="3" sm="6">
+          <v-skeleton-loader
+            class="mx-auto"
+            max-width="300"
+            type="card"
+          ></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" md="6" lg="3" sm="6">
+          <v-skeleton-loader
+            class="mx-auto"
+            max-width="300"
+            type="card"
+          ></v-skeleton-loader>
+        </v-col>
+      </v-row>
+    </v-container>
+
+   <!-- <v-row v-else>
+      <template v-for="(item, i) in lista">
+        <v-col cols="12" md="6" lg="3" sm="6" :key="i">
+          <v-hover v-slot="{ hover }">
+            <v-card :elevation="hover ? 12 : 2" :class="{ 'on-hover': hover }">
+              <v-img :src="item.url" height="225px">
+                <v-card-title class="text-h6 white--text">
+                  <v-row
+                    class="fill-height flex-column"
+                    justify="space-between"
+                  >
+                    <p class="mt-1 subheading  text-left">
+                      {{ item.nombre }}
+                    </p>
+
+                    <div>
+                      <p
+                        class="ma-0 text-body-1 font-weight-bold font-italic text-left"
+                      >
+                        {{ item.nombre }}
+                      </p>
+                      <p
+                        class="text-caption font-weight-medium font-italic text-left"
+                      >
+                        {{ item.nombre }}
+                      </p>
+                    </div>
+
+                    <div class="align-self-center">
+                      <v-btn
+                        v-for="(icon, index) in icons"
+                        :key="index"
+                        :class="{ 'show-btns': hover }"
+                        :color="transparent"
+                        icon
+                      >
+                        <v-icon
+                          :class="{ 'show-btns': hover }"
+                          :color="transparent"
+                        >
+                          {{ icon }}
+                        </v-icon>
+                      </v-btn>
+                    </div>
+                  </v-row>
+                </v-card-title>
+              </v-img>
+            </v-card>
+          </v-hover>
+          -->
+
+          
+          <v-row v-else>
+      <template v-for="(row, i) in lista" >
+      <v-col cols="12" md="6" lg="3" sm="6" :key="i">      
+  
+        <base-card class="overflow-hidden" color="white">
           <v-img cover max-height="240" min-height="240" :src="row.url" />
+          <v-divider class="mx-4 "  color="#91D8DF"></v-divider>          
           <v-card-actions class="pa-4 d-flex justify-space-between">
             <v-card-subtitle class="pa-0">{{ row.nombre }}</v-card-subtitle>
-            <div class="d-flex align-center" v-if="row.count_like > 0">
-              <v-icon class="body-1 mr-1">mdi-cards-heart</v-icon>
-              <v-card-subtitle class="pa-0 mr-2">0k</v-card-subtitle>
-              <!--<v-icon class="body-1 mr-1">mdi-comment-text</v-icon>
-              <v-card-subtitle class="pa-0">900</v-card-subtitle>-->
-            </div>
-          </v-card-actions>
+            <div class="d-flex align-center">
+              <v-icon class="body-1 mr-1"  v-if="row.count_like > 0">mdi-cards-heart</v-icon>
+              <v-card-subtitle class="pa-0 mr-2"  v-if="row.count_like > 0">0k</v-card-subtitle>               
+            </div>            
+          </v-card-actions>          
         </base-card>
-      </v-col>
+
+        </v-col>
+      </template>
     </v-row>
   </v-row>
 </template>
@@ -175,7 +261,7 @@ export default {
       usuarioSesion: {},
       dialog: false,
       currentFile: undefined,
-
+      icons: ['mdi-rewind', 'mdi-pencil'],
       nombre: '',
       descripcion: '',
       errorNombre: false,
@@ -188,6 +274,7 @@ export default {
       lista: [],
       urlPreviewImage: null,
       loading: false,
+      loadingUpload: false,
     }
   },
   mounted() {
@@ -210,11 +297,12 @@ export default {
     async cargarCatalogo() {
       this.loading = true
 
-      this.lista = await this.getAsync(
-        `${URL_API.EJERCICIOS}/${this.usuarioSesion.co_sucursal}`,
-      )
-
-      this.loading = true
+      setTimeout(async () => {
+        this.lista = await this.getAsync(
+          `${URL_API.EJERCICIOS}/${this.usuarioSesion.co_sucursal}`,
+        )
+        this.loading = false
+      }, 700)
     },
     selectFile(file) {
       console.log(file)
@@ -275,3 +363,16 @@ export default {
   },
 }
 </script>
+<style scoped>
+/*.v-card {
+  transition: opacity 0.1s ease-in-out;
+}
+
+.v-card:not(.on-hover) {
+  opacity: 0.6;
+}
+
+.show-btns {
+  color: "#296678" !important;
+}*/
+</style>
