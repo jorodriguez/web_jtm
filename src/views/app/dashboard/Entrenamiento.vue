823dd7ca835f5dc0a72a8c76f7454931d04d7d8d
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2023-06-26 19:31:28 -0500

    cambios

--- src/views/app/dashboard/Entrenamiento.vue
+++ src/views/app/dashboard/Entrenamiento.vue
@@ -4,16 +4,13 @@
       Entrenamiento
     </h4>
 
-    <v-row>
-    
-    </v-row>
+    <v-row></v-row>
 
     <v-row>
       <v-col cols="12" md="6" lg="4" sm="6">
-        <base-card class="h-full">          
+        <base-card class="h-full">
           <v-card-text>
             <v-toolbar color="indigo" dark>
-             
               <v-toolbar-title>Inbox</v-toolbar-title>
 
               <v-spacer />
@@ -26,16 +23,77 @@
                 <v-icon>mdi-dots-vertical</v-icon>
               </v-btn>
             </v-toolbar>
-            <v-list>
-              <v-list-item v-for="item in listaEjercicios" :key="item.id">               
-               <list-card-two
-                                        :avatar="item.url"
-                                        :title="item.nombre"
-                                        :sub-title="item.descripcion"
-                                        primary-btn="View Profile "
-                                        secondary-btn="PROFILE"
-                                    />
-                <!--<v-list-item-avatar tile>
+
+            <v-row>
+              <v-col md="12">
+             
+                <transition-group appear name="fade-up" tag="div">
+                  <v-data-iterator
+                    key
+                    :item="listaEjercicios"
+                    :items-per-page.sync="itemsPerPage"
+                    :page="page"
+                    hide-default-footer
+                  >
+                    <template v-slot:default="props">
+                      <v-row>                      
+                        <v-col
+                          v-for="(item, index) in props.items"
+                          :key="index"
+                          cols="12"
+                          sm="6"
+                          md="6"
+                          lg="4"
+                        >
+                          <base-card class="h-full">
+                            <v-card-text class="d-flex justify-space-between">
+                              <div class="d-flex align-center">
+                                <img
+                                  class="mr-md rounded-circle mr-2"
+                                  height="44"
+                                  width="44"
+                                  :src="item.url"
+                                />
+                                <div>
+                                  <p class="ma-0">
+                                    <a
+                                      href="#"
+                                      class="text--primary font-weight-medium mb-1"
+                                    >
+                                      {{ item.nombre }}
+                                    </a>
+                                  </p>
+                                  <p class="text--disabled caption ma-0">
+                                      {{ item.descripcion }}
+                                  </p>
+                                </div>
+                              </div>
+                              <div>
+                                <v-btn class="ma-2" fab dark color="indigo">
+                                  <v-icon dark>
+                                    mdi-plus
+                                  </v-icon>
+                                </v-btn>
+                              </div>
+                            </v-card-text>
+                          </base-card>
+                        </v-col>
+                      </v-row>
+                    </template>
+                  </v-data-iterator>
+                </transition-group>
+              </v-col>
+            </v-row>
+
+            <!--<list-card-two
+                  :avatar="item.url"
+                  :title="item.nombre"
+                  :sub-title="item.descripcion"
+                  primary-btn="+"
+                  secondary-btn="+"
+                />
+                -->
+            <!--<v-list-item-avatar tile>
                   <v-img :src="item.url" />
                 </v-list-item-avatar>
                 <v-list-item-content>
@@ -46,15 +104,15 @@
                     mdi-star
                   </v-icon>
                 </v-list-item-icon>-->
-              </v-list-item>
-            </v-list>
           </v-card-text>
         </base-card>
       </v-col>
       <v-col cols="12" md="6" lg="4" sm="6">
         <base-card class="h-full"></base-card>
-        </v-col>
-      <v-col cols="12" md="6" lg="4" sm="6"><base-card class="h-full"></base-card></v-col>
+      </v-col>
+      <v-col cols="12" md="6" lg="4" sm="6">
+        <base-card class="h-full"></base-card>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -77,35 +135,35 @@ export default {
   components: {
     'analytic-one-card': analyticOneCard,
     'analytic-two-card': analyticTwoCard,
-     'list-card-two': ListcardTwo
+    'list-card-two': ListcardTwo,
   },
   data() {
     return {
       usuarioSesion: {},
       tabs: null,
-      categorias:[],
-      listaEjercicios:[],
-      listaEjerciciosRespaldo:[],
-      loadingEjercicio:false,
-       itemsTwo: [
-            {
-                icon: true,
-                title: 'Jason Oner',
-                avatar: require('@/assets/images/faces/1.jpg')
-            },
-            {
-                title: 'Travis Howard',
-                avatar: require('@/assets/images/faces/2.jpg')
-            },
-            {
-                title: 'Ali Connors',
-                avatar: require('@/assets/images/faces/3.jpg')
-            },
-            {
-                title: 'Cindy Baker',
-                avatar: require('@/assets/images/faces/4.jpg')
-            }
-        ],
+      categorias: [],
+      listaEjercicios: [],
+      listaEjerciciosRespaldo: [],
+      loadingEjercicio: false,
+      itemsTwo: [
+        {
+          icon: true,
+          title: 'Jason Oner',
+          avatar: require('@/assets/images/faces/1.jpg'),
+        },
+        {
+          title: 'Travis Howard',
+          avatar: require('@/assets/images/faces/2.jpg'),
+        },
+        {
+          title: 'Ali Connors',
+          avatar: require('@/assets/images/faces/3.jpg'),
+        },
+        {
+          title: 'Cindy Baker',
+          avatar: require('@/assets/images/faces/4.jpg'),
+        },
+      ],
     }
   },
   mounted() {
@@ -113,10 +171,9 @@ export default {
     console.log('##### ENTENAMIENTOS ####')
     this.usuarioSesion = getUsuarioSesion()
     this.cargarCatalogo()
-  },   
+  },
   methods: {
     async iniciarCarga() {
-      
       await this.cargarCategorias()
     },
     async cargarCategorias() {
@@ -126,13 +183,13 @@ export default {
       this.loadingEjercicio = true
 
       //setTimeout(async () => {
-        this.listaEjercicios = await this.getAsync(
-          `${URL_API.EJERCICIOS}/${this.usuarioSesion.co_sucursal}`,
-        )
-        this.listaEjerciciosRespaldo = this.lista;
-        this.loadingEjercicio = false
+      this.listaEjercicios = await this.getAsync(
+        `${URL_API.EJERCICIOS}/${this.usuarioSesion.co_sucursal}`,
+      )
+      this.listaEjerciciosRespaldo = this.lista
+      this.loadingEjercicio = false
       //}, 700)
     },
-  }
+  },
 }
 </script>
