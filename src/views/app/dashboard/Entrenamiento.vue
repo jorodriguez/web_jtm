260f956b2f120cf66faf3b2840b7a8ed037326f2
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2023-06-30 15:12:13 -0500

    cambios

--- src/views/app/dashboard/Entrenamiento.vue
+++ src/views/app/dashboard/Entrenamiento.vue
@@ -39,15 +39,49 @@
               <v-col md="12">
                 <v-virtual-scroll
                   :items="listaEjercicios"
-                  :item-height="70"
+                  :item-height="100"
                   height="600"
                 >
                   <template v-slot:default="{ item }">
-                    <div
-                      :draggable="true"
-                      @dragstart="onDragStart(item, $event)"
+                    <v-hover
+                      v-slot="{ hover }"
+                      open-delay="200"
+                      class="item"                      
                     >
-                      <v-list-item>
+                      <div
+                        :draggable="true"
+                        @dragstart="onDragStart(item, $event)"
+                      >
+                        <v-col cols="5" >
+                          <base-card class="overflow-hidden" color="white">
+                            <v-card-text
+                              class="pt-0 pl-2 pb-0 d-flex justify-center"
+                            >
+                              <small class="grey--text">
+                                {{ item.categoria }}
+                              </small>
+                            </v-card-text>
+                            <v-img
+                              cover
+                              max-height="100"
+                              min-height="50"
+                              :src="item.url"
+                            />
+                            <v-divider class="mx-4" color="#91D8DF"></v-divider>
+
+                            <v-card-actions
+                              class="pa-3 d-flex justify-space-between"
+                            >
+                              <v-card-subtitle class="pa-0">
+                                {{ item.nombre }}
+                              </v-card-subtitle>
+                            </v-card-actions>
+                          </base-card>
+                        </v-col>
+                      </div>
+                    </v-hover>
+
+                    <!--                      <v-list-item>
                         <img class="mr-md mr-1" width="60" :src="item.url" />
                         <v-list-item-content>
                           <v-list-item-title>
@@ -56,7 +90,7 @@
                         </v-list-item-content>
                       </v-list-item>
                       <v-divider></v-divider>
-                    </div>
+                      -->
                   </template>
                 </v-virtual-scroll>
               </v-col>
@@ -79,44 +113,59 @@
               </v-btn>
             </v-toolbar>
 
-            <v-row>
-              <v-col md="12">
-                <div class="drop-area" @dragover.prevent @drop="onDrop($event)">
-                  <draggable v-model="listaSeleccion" draggable=".item">
-                    <template v-for="(row, i) in listaSeleccion">
-                      <v-hover v-slot="{ hover }" open-delay="200" class="item" :key="i">
-                        <v-col cols="12" md="6" lg="3" sm="6" :key="i">
-                          <base-card class="overflow-hidden" color="white">
-                            <v-card-text
-                              class="pt-0 pl-2 pb-0 d-flex justify-center"
-                            >
-                              <small class="grey--text">
-                                {{ row.categoria }}
-                              </small>
-                            </v-card-text>
-                            <v-img
-                              cover
-                              max-height="50"
-                              min-height="50"
-                              :src="row.url"
-                            />
-                            <v-divider class="mx-4" color="#91D8DF"></v-divider>
+            <template v-for="(itemCircuito, cindex) in circuitos">
+              <v-row :key="cindex">
+                <v-col md="12">
+                  <div
+                    class="drop-area"
+                    @dragover.prevent
+                    @drop="onDrop($event,cindex)"
+                  >
+                  <h5>Circuito {{cindex}} - }}</h5>
+                    <draggable v-model="circuitos[cindex]" draggable=".item">
+                      <template v-for="(row, i) in circuitoComputed(cindex)">                      
+                        <v-hover
+                          v-slot="{ hover }"
+                          open-delay="200"
+                          class="item"
+                          :key="i"
+                        >
+                          <v-col cols="12" :key="i">
+                            <base-card class="overflow-hidden" color="white">
+                              <v-card-text
+                                class="pt-0 pl-2 pb-0 d-flex justify-center"
+                              >
+                                <small class="grey--text">
+                                  {{ row.categoria }}
+                                </small>
+                              </v-card-text>
+                              <v-img
+                                cover
+                                max-height="50"
+                                min-height="50"
+                                :src="row.url"
+                              />
+                              <v-divider
+                                class="mx-4"
+                                color="#91D8DF"
+                              ></v-divider>
 
-                            <v-card-actions
-                              class="pa-3 d-flex justify-space-between"
-                            >
-                              <v-card-subtitle class="pa-0">
-                                {{ row.nombre }}
-                              </v-card-subtitle>
-                            </v-card-actions>
-                          </base-card>
-                        </v-col>
-                      </v-hover>
-                    </template>
-                  </draggable>
-                </div>
-              </v-col>
-            </v-row>
+                              <v-card-actions
+                                class="pa-3 d-flex justify-space-between"
+                              >
+                                <v-card-subtitle class="pa-0">
+                                  {{ row.nombre }}
+                                </v-card-subtitle>
+                              </v-card-actions>
+                            </base-card>
+                          </v-col>
+                        </v-hover>
+                      </template>
+                    </draggable>
+                  </div>
+                </v-col>
+              </v-row>
+            </template>
           </v-card-text>
         </base-card>
       </v-col>
@@ -149,6 +198,7 @@ export default {
     'list-card-two': ListcardTwo,
     draggable,
   },
+
   data() {
     return {
       usuarioSesion: {},
@@ -166,6 +216,7 @@ export default {
       filter: {},
       sortBy: 'nombre',
       ejercicioSeleccionado: null,
+      circuitos: [],
     }
   },
   mounted() {
@@ -173,8 +224,15 @@ export default {
     console.log('##### ENTENAMIENTOS ####')
     this.usuarioSesion = getUsuarioSesion()
     this.cargarCatalogo()
+    this.circuitos[1] = new Array();
+    this.circuitos[2] = new Array();
+    this.circuitos[3] = new Array();
+    this.circuitos[4] = new Array();
   },
   computed: {
+    circuitoComputed(index) {
+      return this.circuitos[index];
+    },
     numberOfPages() {
       return Math.ceil(this.listaEjercicios.length / this.itemsPerPage)
     },
@@ -271,12 +329,30 @@ export default {
       console.log(item)
       event.dataTransfer.setData('text/plain', JSON.stringify(item))
     },
-    onDrop(event) {
-      console.log('onDrop')
+    onDrop(event, indexCircuito) {
+      console.log('onDrop INDEX '+indexCircuito)
       const data = event.dataTransfer.getData('text/plain')
       const item = JSON.parse(data)
       console.log(item)
-      this.listaSeleccion.push({ ...item })
+
+      //let circuito  = this.circuitos[indexCircuito];
+
+      if(!this.circuitos[indexCircuito]){
+        console.log("Se crea el array");
+        this.circuitos[indexCircuito] = new Array();
+      }
+                  
+      this.circuitos[indexCircuito].push({ ...item })
+
+      console.log(JSON.stringify(this.circuitos[indexCircuito]))
+
+      //circuito.push({ ...item });
+
+      //this.circuitos[indexCircuito] = circuito;
+
+      //this.circuitos[indexCircuito].push({ ...item })
+
+      //this.listaSeleccion.push({ ...item })
     },
   },
 }
